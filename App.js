d4967c2876f470e0784fe6dea67bc5c8bcb5d925
goki sakaguchi <goki0524@gmail.com>
2019-05-18 17:52:41 +0900

    Add function login, signup
    メールアドレスでのログイン・サインアップ機能追加。

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
-import * as firebase from 'firebase';
-import firebaseConfig from './config/firebase/firebaseConfig.json';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base'
+import * as firebase from 'firebase'
+import firebaseConfig from './config/firebase/firebaseConfig.json'
 
 export default class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = ({
+      email: '',
+      password: ''
+    })
+  }
+
   componentWillMount() {
     // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+    firebase.initializeApp(firebaseConfig)
+  }
+
+  signUpUser = (email, password) => {
+    try {
+      if(this.state.password.length < 6){
+        alert("パスワードは6文字以上で登録してください。")
+        return
+      }
+
+      firebase.auth().createUserWithEmailAndPassword(email, password)
+    }
+    catch (error) {
+      console.log(error.toString())
+    }
+  }
+
+  loginUser = (email, password) => {
+    try {
+      firebase.auth().signInWithEmailAndPassword(email, password).then((user)=>{
+        console.log(user)
+      })
+    }
+    catch (error) {
+      console.log(error.toString())
+    }
   }
 
   render() {
@@ -19,6 +54,7 @@ export default class App extends React.Component {
               <Input
                 autoCorrect={false}
                 autoCapitalize="none"
+                onChangeText={(email)=> this.setState({email})}
               />
           </Item>
 
@@ -28,6 +64,7 @@ export default class App extends React.Component {
               secureTextEntry={true}
               autoCorrect={false}
               autoCapitalize="none"
+              onChangeText={(password)=> this.setState({password})}
             />
           </Item>
 
@@ -35,12 +72,22 @@ export default class App extends React.Component {
             full
             rounded
             success
+            onPress={()=> this.loginUser(this.state.email, this.state.password)}
           >
-            <Text>ログイン</Text>
+            <Text style={{color: 'white'}}>ログイン</Text>
+          </Button>
+
+          <Button style={{marginTop: 20}}
+            full
+            rounded
+            primary
+            onPress={()=> this.signUpUser(this.state.email, this.state.password)}
+          >
+            <Text style={{color: 'white'}}>アカウント作成</Text>
           </Button>
         </Form>
       </Container>
-    );
+    )
   }
 }
 
@@ -51,4 +98,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     padding: 10,
   },
-});
+})
