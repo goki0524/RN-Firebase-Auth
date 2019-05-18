b026e322abbc5ca45d34e6cc6d7aa9d84d88ea59
goki sakaguchi <goki0524@gmail.com>
2019-05-18 16:55:20 +0900

    firebase設定とログインフォーム追加

--- App.js
+++ App.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Container, Content, Header, Form, Input, Item, Button, Label } from 'native-base';
+import * as firebase from 'firebase';
+import firebaseConfig from './config/firebase/firebaseConfig.json';
 
 export default class App extends React.Component {
+  componentWillMount() {
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <Container style={styles.container}>
+        <Form>
+          <Item floatingLabel>
+              <Label>Email</Label>
+              <Input
+                autoCorrect={false}
+                autoCapitalize="none"
+              />
+          </Item>
+
+          <Item floatingLabel>
+            <Label>Password</Label>
+            <Input
+              secureTextEntry={true}
+              autoCorrect={false}
+              autoCapitalize="none"
+            />
+          </Item>
+
+          <Button style={{marginTop: 50}}
+            full
+            rounded
+            success
+          >
+            <Text>ログイン</Text>
+          </Button>
+        </Form>
+      </Container>
     );
   }
 }
@@ -15,7 +48,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 10,
   },
 });
